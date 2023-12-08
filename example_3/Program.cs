61d733e7d002565d1d10399ba212b6d8d9e55553
Anton <2004lol222@mail.ru>
2023-12-08 22:09:48 +0800

    задача 3

---
+++ example_3/Program.cs
@@ -0,0 +1,11 @@
+﻿int r = new Random().Next(10, 99);
+
+int p1 = r / 10; //Первое число
+int p2 = r % 10; // последние число
+
+Console.WriteLine("рандомные число: " + r);
+Console.Write("Наибольшее из них : ");
+if (p1 > p2) {Console.WriteLine(p1);}
+else {Console.WriteLine(p2);}
+
+
