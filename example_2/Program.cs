b8b3fbe1c2890f95a32b2baca4abbfdd36fd13d3
Anton <2004lol222@mail.ru>
2023-12-08 21:38:46 +0800

    2 задача

---
+++ example_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число 1: ");
+int X = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int Y = int.Parse(Console.ReadLine());
+
+if (X != 0 && Y != 0) {
+    if (X > 0 && Y > 0) {Console.WriteLine("1 четверть");}
+    if (X < 0 && Y > 0) {Console.WriteLine("2 четверть");}
+    if (X < 0 && Y < 0) {Console.WriteLine("3 четверть");}
+    if (X > 0 && Y < 0) {Console.WriteLine("4 четверть");}
+    
+}else {Console.WriteLine("Число не должно равняться 0");}
